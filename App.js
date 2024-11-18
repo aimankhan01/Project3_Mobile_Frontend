9070292e7fce5602c4b509ab4771df1bc6fff592
Aiman <khanaimanwasim111@gmail.com>
2024-11-17 18:42:18 -0800

    Login Page

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginPage from './src/pages/LoginPage';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login"
+      screenOptions={{
+        headerShown: false, // Hide header for all screens in the stack
+      }}>
+        <Stack.Screen name="Login" component={LoginPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
